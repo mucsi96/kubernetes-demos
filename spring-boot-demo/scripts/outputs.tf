ec73df744bf58ba8a0be990d16ae591145597b35
Igor Bari <mucsi96@gmail.com>
2022-09-17 15:21:01 +0200

    debug

--- spring-boot-demo/scripts/outputs.tf
+++ spring-boot-demo/scripts/outputs.tf
@@ -1,3 +1,13 @@
 output "runNumber" {
   value = var.runNumber
 }
+
+output "dockerConfig" {
+  value = jsonencode({
+    auths = {
+      "https://index.docker.io/v1/" = {
+        auth = var.dockerAccessToken
+      }
+    }
+  })
+}
