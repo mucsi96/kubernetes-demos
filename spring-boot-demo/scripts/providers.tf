7d79b5595438ba1ccd8f081a0191ff1a52fec0c1
Igor Bari <mucsi96@gmail.com>
2022-09-17 15:15:36 +0200

    try jsonencode

--- spring-boot-demo/scripts/providers.tf
+++ spring-boot-demo/scripts/providers.tf
@@ -10,6 +10,12 @@ terraform {
 provider "docker" {
   registry_auth {
     address             = "registry-1.docker.io"
-    config_file_content = "{\"auths\":{\"https://index.docker.io/v1/\":{\"username\":\"${var.dockerUsername}\",\"auth\":\"${var.dockerAccessToken}\"}}}"
+    config_file_content = jsonencode({
+      "auths": {
+        "https://index.docker.io/v1/": {
+          "auth": var.dockerAccessToken
+        }
+      }
+    })
   }
 }
