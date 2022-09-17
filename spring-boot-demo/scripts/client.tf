e4d5699fcd90f36331cf041dc792c4855997b63e
Igor Bari <mucsi96@gmail.com>
2022-09-17 18:11:25 +0200

    try without docker_image

--- spring-boot-demo/scripts/client.tf
+++ spring-boot-demo/scripts/client.tf
@@ -1,59 +1,30 @@
 locals {
-    image_name = "${var.dockerUsername}/spring-boot-demo"
+    image_name = "${var.dockerUsername}/spring-boot-demo-client"
 }
 
-# resource "null_resource" "docker_build" {
+# resource "docker_image" "client" {
+#   name = "${local.image_name}:${var.runNumber}"
 
-#     triggers = {
-#         always_run = timestamp()
-#     }
+#   build {
+#     path = "../client"
+#     tag  = ["${local.image_name}:latest"]
+#   }
 
-#     provisioner "local-exec" {
-#         command = "docker build -t ${local.image_name}:latest -t ${local.image_name}:${var.runNumber} ../client"
-#     }
+#   triggers = {
+#     always_run = timestamp()
+#   }
 # }
 
-# resource "null_resource" "docker_login" {
-
-#     depends_on = [ null_resource.docker_build ]
-
-#     triggers = {
-#         always_run = timestamp()
-#     }
-
-#     provisioner "local-exec" {
-#         command = "docker login --username ${var.dockerUsername} --password ${var.dockerAccessToken}"
-#     }
-# }
-
-# resource "null_resource" "docker_push" {
-
-#     depends_on = [ null_resource.docker_login ]
-
-#     triggers = {
-#         always_run = timestamp()
-#     }
-
-#     provisioner "local-exec" {
-#         command = "docker push ${local.image_name}"
-#     }
-# }
-
-resource "docker_image" "client" {
+resource "docker_registry_image" "client" {
   name = "${local.image_name}:${var.runNumber}"
 
   build {
-    path = "../client"
-    tag  = ["${local.image_name}:latest"]
-  }
-
-  triggers = {
-    always_run = timestamp()
+    context = abspath("../client")
   }
 }
 
 resource "docker_registry_image" "client" {
-  name = "${local.image_name}:${var.runNumber}"
+  name = "${local.image_name}:latest"
 
   build {
     context = abspath("../client")
