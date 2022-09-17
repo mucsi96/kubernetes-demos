365fafebcaf8d179562152b334c7f9a88a9569f6
Igor Bari <mucsi96@gmail.com>
2022-09-17 22:50:29 +0200

    improve scripts

--- spring-boot-demo/kubernetes-dashboard.sh
+++ spring-boot-demo/kubernetes-dashboard.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 POD_NAME=$(kubectl get pods | awk '{print $1}' | grep kubernetes-dashboard)
-echo -------------------------------------------------- TOKEN --------------------------------------------------
-kubectl create token kubernetes-dashboard
-echo -----------------------------------------------------------------------------------------------------------
+kubectl create token kubernetes-dashboard | clip
+echo "Your token is in clipboard."
 start chrome http://localhost:9090
 kubectl -n default port-forward $POD_NAME 9090:9090
