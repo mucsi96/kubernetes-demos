cd000704b62317b9fb4b661b03dca959a2e3a471
Igor Bari <mucsi96@gmail.com>
2022-09-17 15:39:42 +0200

    try tf template

--- spring-boot-demo/scripts/providers.tf
+++ spring-boot-demo/scripts/providers.tf
@@ -10,12 +10,14 @@ terraform {
 provider "docker" {
   registry_auth {
     address             = "registry-1.docker.io"
-    config_file_content = jsonencode({
-      auths = {
-        "https://index.docker.io/v1/" = {
-          auth = var.dockerAccessToken
+    config_file_content = <<EOT
+      {
+        "auths": {
+          "https://index.docker.io/v1/": {
+            "auth": "${var.dockerAccessToken}"
+          }
         }
       }
-    })
+      EOT
   }
 }
