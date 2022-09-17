25011c9315e1848bfc5663324d027e57e730ccbb
Igor Bari <mucsi96@gmail.com>
2022-09-17 13:00:59 +0200

    add spaces

--- spring-boot-demo/scripts/main.tf
+++ spring-boot-demo/scripts/main.tf
@@ -1,9 +1,11 @@
 resource "docker_image" "client" {
   name = "spring-boot-demo-client:latest"
+
   build {
     path = "../client"
     tag  = ["spring-boot-demo-client:${var.runNumber}"]
   }
+  
   triggers = {
     always_run = timestamp()
     # dir_sha1 = sha1(join("", [for f in fileset(path.module, "../client/**") : filesha1(f)]))
