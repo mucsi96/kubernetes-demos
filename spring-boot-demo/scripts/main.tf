6cbac02be27e0a14d37837cb2610123c655d1018
Igor Bari <mucsi96@gmail.com>
2022-09-17 23:09:09 +0200

    add depends on

--- spring-boot-demo/scripts/main.tf
+++ spring-boot-demo/scripts/main.tf
@@ -1,4 +1,10 @@
 resource "helm_release" "install" {
   name       = "spring-boot-demo"
   chart      = ".."
+  depends_on = [
+    docker_registry_image.client_with_version,
+    docker_registry_image.client_latest,
+    docker_registry_image.server_with_version,
+    docker_registry_image.server_latest
+  ]
 }
