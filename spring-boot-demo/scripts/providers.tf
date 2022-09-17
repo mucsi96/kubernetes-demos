a534b75d73df757d3b4c169d5b6960c02213becd
Igor Bari <mucsi96@gmail.com>
2022-09-17 16:04:33 +0200

    revert

--- spring-boot-demo/scripts/providers.tf
+++ spring-boot-demo/scripts/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "docker" {
   registry_auth {
-    address     = "https://index.docker.io/v1/"
+    address     = "registry-1.docker.io"
     config_file = pathexpand("~/.docker/config.json")
   }
 }
