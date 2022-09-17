f698e2e6d8658d97cf300cf441187bd9d1762d30
Igor Bari <mucsi96@gmail.com>
2022-09-17 15:17:05 +0200

    try 3

--- spring-boot-demo/scripts/providers.tf
+++ spring-boot-demo/scripts/providers.tf
@@ -11,9 +11,9 @@ provider "docker" {
   registry_auth {
     address             = "registry-1.docker.io"
     config_file_content = jsonencode({
-      "auths": {
-        "https://index.docker.io/v1/": {
-          "auth": var.dockerAccessToken
+      auths = {
+        "https://index.docker.io/v1/" = {
+          auth = var.dockerAccessToken
         }
       }
     })
