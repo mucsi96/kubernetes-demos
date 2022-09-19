2e0cc0dfabe48b1e2689a5d26bb6136dc337e8ff
Igor Bari <mucsi96@gmail.com>
2022-09-19 08:24:22 +0200

    remove namespace from kubernetes_cluster_role_binding

--- spring-boot-demo/scripts/kubernetes-dashboard.tf
+++ spring-boot-demo/scripts/kubernetes-dashboard.tf
@@ -25,7 +25,6 @@ resource "helm_release" "kubernetes-dashboard" {
 resource "kubernetes_cluster_role_binding" "kubernetes-dashboard" {
   metadata {
     name      = "kubernetes-dashboard"
-    namespace = "kubernetes-dashboard"
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
