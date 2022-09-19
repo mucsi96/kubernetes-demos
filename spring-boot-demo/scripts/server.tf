c469fa258e5907611b6e38dc1612c385c59b5536
Igor Bari <mucsi96@gmail.com>
2022-09-19 08:55:36 +0200

    deploy server

--- spring-boot-demo/scripts/server.tf
+++ spring-boot-demo/scripts/server.tf
@@ -14,3 +14,43 @@ resource "null_resource" "docker_server" {
     always_run = timestamp()
   }
 }
+
+resource "helm_release" "server" {
+  name             = "server"
+  namespace        = var.app_namespace
+  create_namespace = true
+  chart            = "../charts/server"
+  depends_on = [
+    null_resource.docker_server
+  ]
+
+  set {
+    name  = "image.tag"
+    value = var.run_number
+  }
+
+  set {
+    name  = "service.port"
+    value = var.server_port
+  }
+
+  set {
+    name  = "database.name"
+    value = var.app_namespace
+  }
+
+  set {
+    name  = "database.host"
+    value = "database"
+  }
+
+  set {
+    name  = "database.userName"
+    value = var.database_user_name
+  }
+
+  set {
+    name  = "database.password"
+    value = var.database_password
+  }
+}
