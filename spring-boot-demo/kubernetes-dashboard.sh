e9bda7eb28e1297a15709931c3521b5616e69c49
Igor Bari <mucsi96@gmail.com>
2022-09-19 08:48:44 +0200

    fix dashboard script

--- spring-boot-demo/kubernetes-dashboard.sh
+++ spring-boot-demo/kubernetes-dashboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POD_NAME=$(kubectl get pods | awk '{print $1}' | grep kubernetes-dashboard)
-kubectl create token kubernetes-dashboard | clip
-echo "Your token is in clipboard."
-start chrome http://localhost:9090
-kubectl -n default port-forward $POD_NAME 9090:9090
+POD_NAME=$(kubectl get pods --namespace kubernetes-dashboard | awk '{print $1}' | grep kubernetes-dashboard)
+# kubectl create token kubernetes-dashboard --namespace kubernetes-dashboard | clip
+# echo "Your token is in clipboard."
+start chrome http://localhost:9090/\#/pod?namespace=spring-boot-demo
+kubectl port-forward $POD_NAME 9090:9090 --namespace kubernetes-dashboard
