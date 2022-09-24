a984f5dc10914c8a7c34e64a7a0d5871f999990d
Igor Bari <mucsi96@gmail.com>
2022-09-24 14:32:08 +0200

    remove variables

--- spring-boot-demo/scripts/modules/monitoring/prometheus.tf
+++ spring-boot-demo/scripts/modules/monitoring/prometheus.tf
@@ -6,28 +6,28 @@ resource "helm_release" "prometheus" {
   repository       = "https://prometheus-community.github.io/helm-charts"
   version          = "15.13.0"
 
-  set {
-    name  = "server.resources.limits.cpu"
-    value = "200m"
-  }
+  # set {
+  #   name  = "server.resources.limits.cpu"
+  #   value = "200m"
+  # }
 
-  set {
-    name  = "server.resources.limits.memory"
-    value = "50Mi"
-  }
+  # set {
+  #   name  = "server.resources.limits.memory"
+  #   value = "50Mi"
+  # }
 
-  set {
-    name  = "server.resources.requests.cpu"
-    value = "100m"
-  }
+  # set {
+  #   name  = "server.resources.requests.cpu"
+  #   value = "100m"
+  # }
 
-  set {
-    name  = "server.resources.requests.memory"
-    value = "30Mi"
-  }
+  # set {
+  #   name  = "server.resources.requests.memory"
+  #   value = "30Mi"
+  # }
 
-  set {
-    name  = "alertmanager.enabled"
-    value = false
-  }
+  # set {
+  #   name  = "alertmanager.enabled"
+  #   value = false
+  # }
 }
