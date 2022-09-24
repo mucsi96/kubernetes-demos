3256c090757b0366676d43d7a25f31cb89a66911
Igor Bari <mucsi96@gmail.com>
2022-09-24 22:01:29 +0200

    add traefik-2_rev1.json dashboard

--- spring-boot-demo/scripts/modules/monitoring/main.tf
+++ spring-boot-demo/scripts/modules/monitoring/main.tf
@@ -6,3 +6,17 @@ resource "helm_release" "kube-prometheus-stack" {
   repository       = "https://prometheus-community.github.io/helm-charts"
   version          = "40.1.2"
 }
+
+resource "kubernetes_config_map" "grafana-dashboards" {
+  metadata {
+    name      = "grafana-dashboards"
+    namespace = var.namespace
+    labels = {
+      grafana_dashboard = 1
+    }
+  }
+
+  data = {
+    "traefik-2_rev1.json" = file("${path.module}/dashboards/traefik-2_rev1.json")
+  }
+}
