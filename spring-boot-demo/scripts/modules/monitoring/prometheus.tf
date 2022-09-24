daf3f48be8634ffd575e4b4e1218ccc4e10c5333
Igor Bari <mucsi96@gmail.com>
2022-09-24 14:41:55 +0200

    increase prometheus limits

--- spring-boot-demo/scripts/modules/monitoring/prometheus.tf
+++ spring-boot-demo/scripts/modules/monitoring/prometheus.tf
@@ -6,25 +6,25 @@ resource "helm_release" "prometheus" {
   repository       = "https://prometheus-community.github.io/helm-charts"
   version          = "15.13.0"
 
-  # set {
-  #   name  = "server.resources.limits.cpu"
-  #   value = "200m"
-  # }
+  set {
+    name  = "server.resources.limits.cpu"
+    value = "400m"
+  }
 
-  # set {
-  #   name  = "server.resources.limits.memory"
-  #   value = "50Mi"
-  # }
+  set {
+    name  = "server.resources.limits.memory"
+    value = "500Mi"
+  }
 
-  # set {
-  #   name  = "server.resources.requests.cpu"
-  #   value = "100m"
-  # }
+  set {
+    name  = "server.resources.requests.cpu"
+    value = "100m"
+  }
 
-  # set {
-  #   name  = "server.resources.requests.memory"
-  #   value = "30Mi"
-  # }
+  set {
+    name  = "server.resources.requests.memory"
+    value = "30Mi"
+  }
 
   # set {
   #   name  = "alertmanager.enabled"
