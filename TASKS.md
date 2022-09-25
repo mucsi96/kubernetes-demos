0ff5fdf982c8ca0e1bdc7c09f9b0a089e1eec749
Igor Bari <mucsi96@gmail.com>
2022-09-25 07:34:25 +0200

    add traefik_servicemonitor

--- TASKS.md
+++ TASKS.md
@@ -10,3 +10,4 @@
 - Create new repo for cluster
 - Add traefik meterics to grafana dashboard https://github.com/cablespaghetti/k3s-monitoring
 - Try versioning without patching charts
+- Connect modules using output's 
--- spring-boot-demo/scripts/modules/monitoring/main.tf
+++ spring-boot-demo/scripts/modules/monitoring/main.tf
@@ -7,7 +7,7 @@ resource "helm_release" "kube-prometheus-stack" {
   version          = "40.1.2"
 }
 
-resource "kubernetes_config_map" "grafana-dashboards" {
+resource "kubernetes_config_map" "grafana_dashboards" {
   metadata {
     name      = "grafana-dashboards"
     namespace = var.namespace
@@ -18,5 +18,27 @@ resource "kubernetes_config_map" "grafana-dashboards" {
 
   data = {
     "traefik-2_rev1.json" = file("${path.module}/dashboards/traefik-2_rev1.json")
+  }
+}
+
+resource "kubernetes_manifest" "traefik_servicemonitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "ServiceMonitor"
+    metadata = {
+      name      = "traefik"
+      namespace = var.namespace
+    }
+    spec = {
+      endpoints = [{ port : metrics }]
+      namespaceSelector = {
+        matchNames = "kube-system"
+      }
+      selector = {
+        matchLabels = {
+          app = "traefik"
+        }
+      }
+    }
   }
 }
