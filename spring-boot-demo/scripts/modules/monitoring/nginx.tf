53f1cc896a8812456de05a864c0d5c2ac91bc277
Igor Bari <mucsi96@gmail.com>
2022-09-25 14:51:34 +0200

    use prometheus annotations

--- spring-boot-demo/scripts/modules/monitoring/nginx.tf
+++ spring-boot-demo/scripts/modules/monitoring/nginx.tf
@@ -1,32 +1,3 @@
-# CRD Ref: https://prometheus-operator.dev/docs/operator/api/#monitoring.coreos.com/v1.PodMonitor
-resource "kubernetes_manifest" "client_service_monitor" {
-  manifest = {
-    apiVersion = "monitoring.coreos.com/v1"
-    kind       = "PodMonitor"
-    metadata = {
-      name      = "client"
-      namespace = var.namespace
-      labels = {
-        "release" = helm_release.kube-prometheus-stack.name
-      }
-    }
-    spec = {
-      podMetricsEndpoints = [{
-        port = "metrics"
-        path = "/metrics"
-      }]
-      namespaceSelector = {
-        matchNames = [var.client.namespace]
-      }
-      selector = {
-        matchLabels = {
-          "app.kubernetes.io/instance" = var.client.name
-        }
-      }
-    }
-  }
-}
-
 resource "kubernetes_config_map" "nginx_dashboard" {
   metadata {
     name      = "nginx-dashboard"
