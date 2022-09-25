ecfa935e6c7384e4454e66f646ad0c14cc8ecba5
Igor Bari <mucsi96@gmail.com>
2022-09-25 10:03:26 +0200

    add nginx monitoring

--- spring-boot-demo/scripts/modules/monitoring/main.tf
+++ spring-boot-demo/scripts/modules/monitoring/main.tf
@@ -7,17 +7,81 @@ resource "helm_release" "kube-prometheus-stack" {
   version          = "40.1.2"
 }
 
-resource "kubernetes_config_map" "grafana_dashboards" {
+resource "helm_release" "nginx-prometheus-exporter" {
+  chart            = "nginx-prometheus-exporter"
+  name             = "nginx-prometheus-exporter"
+  namespace        = var.namespace
+  create_namespace = true
+  repository       = "https://nexclipper.github.io/helm-charts"
+  version          = "0.1.3"
+
+  set {
+    name  = "nginxServer"
+    value = "http://app-client.spring-boot-demo.svc.cluster.local:8080/stub_status"
+  }
+
+  set {
+    name = "serviceMonitor.labels"
+    value = {
+      "release" = helm_release.kube-prometheus-stack.name
+    }
+  }
+}
+
+resource "kubernetes_config_map" "traefik_dashboard" {
   metadata {
-    name      = "grafana-dashboards"
+    name      = "traefik-dashboard"
     namespace = var.namespace
     labels = {
-      grafana_dashboard = 1
+      grafana_dashboard = true
     }
   }
 
   data = {
     "traefik-2_rev1.json" = file("${path.module}/dashboards/traefik-2_rev1.json")
+  }
+}
+
+resource "kubernetes_config_map" "nginx_dashboard" {
+  metadata {
+    name      = "nginx-dashboard"
+    namespace = var.namespace
+    labels = {
+      grafana_dashboard = true
+    }
+  }
+
+  data = {
+    "nginx_rev1.json" = file("${path.module}/dashboards/nginx_rev1.json")
+  }
+}
+
+# CRD Ref: https://prometheus-operator.dev/docs/operator/api/#monitoring.coreos.com/v1.PodMonitor
+resource "kubernetes_manifest" "traefik_pod_monitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "PodMonitor"
+    metadata = {
+      name      = "traefik"
+      namespace = var.namespace
+      labels = {
+        "release" = helm_release.kube-prometheus-stack.name
+      }
+    }
+    spec = {
+      podMetricsEndpoints = [{
+        port = "metrics"
+        path = "/metrics"
+      }]
+      namespaceSelector = {
+        matchNames = ["kube-system"]
+      }
+      selector = {
+        matchLabels = {
+          "app.kubernetes.io/name" = "traefik"
+        }
+      }
+    }
   }
 }
 
