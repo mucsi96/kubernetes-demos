c2e00b63b8222976d8b33220d7705dcaa836aaed
Igor Bari <mucsi96@gmail.com>
2022-09-27 21:53:52 +0200

    fix typo

--- spring-boot-demo/scripts/modules/helm-chart-version/main.tf
+++ spring-boot-demo/scripts/modules/helm-chart-version/main.tf
@@ -7,9 +7,9 @@ module "chart_version" {
 resource "null_resource" "command" {
   provisioner "local-exec" {
     command = <<-EOT
-      cd ${abspath(var.chart_path)}
-       sed -i "s/^appVersion:.*$/appVersion: \"${var.app_version}\"/" Chart.yaml
-       sed -i "s/^version:.*$/version: \"${module.chart_version.version}.${var.app_version}\"/" Chart.yaml
+      cd ${abspath(var.path)}
+      sed -i "s/^appVersion:.*$/appVersion: \"${var.app_version}\"/" Chart.yaml
+      sed -i "s/^version:.*$/version: \"${module.chart_version.version}.${var.app_version}\"/" Chart.yaml
     EOT
   }
 }
