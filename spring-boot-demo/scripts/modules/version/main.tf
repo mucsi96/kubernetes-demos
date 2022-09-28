8e4408adaa3817f690d1659377f95bc98cb227b2
Igor Bari <mucsi96@gmail.com>
2022-09-28 09:04:28 +0200

    turn logs on

--- spring-boot-demo/scripts/modules/version/main.tf
+++ spring-boot-demo/scripts/modules/version/main.tf
@@ -5,11 +5,3 @@ data "external" "version" {
     tag_prefix = var.tag_prefix
   }
 }
-
-resource "null_resource" "debug" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      echo "Output ${data.external.version.result}"
-    EOT
-  }
-}
