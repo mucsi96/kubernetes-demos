eea557e52113982d09fd8af09f0a9c40793e34d8
Igor Bari <mucsi96@gmail.com>
2022-09-28 07:25:16 +0200

    add bash as prefix

--- spring-boot-demo/scripts/modules/version/main.tf
+++ spring-boot-demo/scripts/modules/version/main.tf
@@ -1,5 +1,5 @@
 data "external" "version" {
-  program     = ["${path.module}/command.sh"]
+  program     = ["bash", "${path.module}/command.sh"]
   working_dir = var.path
   query = {
     tag_prefix = var.tag_prefix
