e7844e283360a4611e531bce575865e133739816
Igor Bari <mucsi96@gmail.com>
2022-09-28 09:02:24 +0200

    add debug output

--- spring-boot-demo/scripts/modules/version/main.tf
+++ spring-boot-demo/scripts/modules/version/main.tf
@@ -5,3 +5,11 @@ data "external" "version" {
     tag_prefix = var.tag_prefix
   }
 }
+
+resource "null_resource" "debug" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "Output ${data.external.version.result}"
+    EOT
+  }
+}
