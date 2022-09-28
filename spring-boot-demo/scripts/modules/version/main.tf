2034e5a9783d9390da42c382090d71eaf0f58eed
Igor Bari <mucsi96@gmail.com>
2022-09-28 07:35:01 +0200

    add permission

--- spring-boot-demo/scripts/modules/version/main.tf
+++ spring-boot-demo/scripts/modules/version/main.tf
@@ -1,5 +1,5 @@
 data "external" "version" {
-  program     = ["bash", abspath("${path.module}/command.sh")]
+  program     = [abspath("${path.module}/command.sh")]
   working_dir = var.path
   query = {
     tag_prefix = var.tag_prefix
