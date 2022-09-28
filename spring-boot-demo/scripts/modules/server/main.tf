20353a8c1a6926797c8e1667a58df2ffe4033e5d
Igor Bari <mucsi96@gmail.com>
2022-09-28 17:03:14 +0200

    remove version script

--- spring-boot-demo/scripts/modules/server/main.tf
+++ spring-boot-demo/scripts/modules/server/main.tf
@@ -1,9 +1,14 @@
-data "local_file" "image_version" {
-  filename = "../server/version.txt"
+module "image_version" {
+  source     = "../version"
+  tag_prefix = "server-image"
+  path       = "../server"
 }
 
-data "local_file" "chart_version" {
-  filename = "../charts/server/version.txt"
+module "chart_version" {
+  source      = "../helm-chart-version"
+  app_version = module.image_version.version
+  tag_prefix  = "server-chart"
+  path        = "../charts/server"
 }
 
 resource "null_resource" "image" {
@@ -11,21 +16,21 @@ resource "null_resource" "image" {
     command = <<-EOT
       docker build ../server \
         --quiet \
-        --tag ${var.image_name}:${data.local_file.image_version.content} \
+        --tag ${var.image_name}:${module.image_version.version} \
         --tag ${var.image_name}:latest
       docker push ${var.image_name} --all-tags
-      echo "New image published ${var.image_name}:${data.local_file.image_version.content}"
+      echo "New image published ${var.image_name}:${module.image_version.version}"
     EOT
   }
 
   triggers = {
-    version = data.local_file.image_version.content
+    version = module.image_version.version
   }
 }
 
 resource "helm_release" "chart" {
   name             = var.host
-  version          = data.local_file.chart_version.content
+  version          = module.chart_version.version
   namespace        = var.namespace
   create_namespace = true
   chart            = "../charts/server"
--- spring-boot-demo/scripts/modules/spring-boot-admin/main.tf
+++ spring-boot-demo/scripts/modules/spring-boot-admin/main.tf
@@ -1,9 +1,14 @@
-data "local_file" "image_version" {
-  filename = "../spring-boot-admin/version.txt"
+module "image_version" {
+  source     = "../version"
+  tag_prefix = "spring-boot-admin-image"
+  path       = "../spring-boot-admin"
 }
 
-data "local_file" "chart_version" {
-  filename = "../charts/spring-boot-admin/version.txt"
+module "chart_version" {
+  source      = "../helm-chart-version"
+  app_version = module.image_version.version
+  tag_prefix  = "spring-boot-admin-chart"
+  path        = "../charts/spring-boot-admin"
 }
 
 resource "null_resource" "image" {
@@ -11,21 +16,21 @@ resource "null_resource" "image" {
     command = <<-EOT
       docker build ../spring-boot-admin \
         --quiet \
-        --tag ${var.image_name}:${data.local_file.image_version.content} \
+        --tag ${var.image_name}:${module.image_version.version} \
         --tag ${var.image_name}:latest
       docker push ${var.image_name} --all-tags
-      echo "New image published ${var.image_name}:${data.local_file.image_version.content}"
+      echo "New image published ${var.image_name}:${module.image_version.version}"
     EOT
   }
 
   triggers = {
-    version = data.local_file.image_version.content
+    version = module.image_version.version
   }
 }
 
 resource "helm_release" "chart" {
   name             = var.host
-  version          = data.local_file.chart_version.content
+  version          = module.chart_version.version
   namespace        = var.namespace
   create_namespace = true
   chart            = "../charts/spring-boot-admin"
