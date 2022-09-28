a4d29379cbe5ee462c348f8b69ac2f447f196f65
Igor Bari <mucsi96@gmail.com>
2022-09-28 22:54:06 +0200

    git diff based versioning

--- spring-boot-demo/scripts/modules/version/command.sh
+++ spring-boot-demo/scripts/modules/version/command.sh
@@ -12,43 +12,28 @@ then
     exit 2
 fi
 
-prev_commit=
-prev_sha=
 prev_tag=$(git describe --tags --match=$tag_prefix-*-* --abbrev=0)
-retVal=$?
-if [ $retVal -eq 0 ]
+if [ $? -eq 0 ]
 then
-    prev_commit=$(git show $prev_tag | head -1 | cut -d " " -f 2)
-    prev_sha=$(echo "$prev_tag" | sed "s/^$tag_prefix-//" | cut -d "-" -f 2)
-fi
-current_commit=$(git show HEAD | head -1 | cut -d " " -f 2)
-current_sha=$(git ls-files -z | sed 's/^/\.\//' -z | sort -z | xargs -0 sha1sum | sha1sum | awk '{print $1}' | cut -c 1-8)
-
-if [[ "$current_commit" == "$prev_commit" ]]
-then
-    version=$(echo "$prev_tag" | sed "s/^$tag_prefix-//" | cut -d "-" -f 1)
-    jq -n --arg version "$version" '{"version":$version}'
-    exit 0
+    git diff --quiet HEAD $prev_tag -- .
+    if [ $? -eq 0 ]
+    then
+        version=$(echo "$prev_tag" | sed "s/^$tag_prefix-//")
+        jq -n --arg version "$version" '{"version":$version}'
+        exit 0
+    fi
 fi
 
-latest_version=$(git tag --list --sort=taggerdate $tag_prefix-*-* | tail -1 | sed "s/^$tag_prefix-//" | cut -d "-" -f 1)
+latest_version=$(git tag --list --sort=taggerdate $tag_prefix-*-* | tail -1 | sed "s/^$tag_prefix-//")
 new_version=
 if [[ -z "$latest_version" ]]
 then
     new_version=1
 else
-    if [[ "$current_sha" == "$prev_sha" ]]
-    then
-        new_version=$latest_version
-    else
-        new_version=$((latest_version + 1))
-    fi
+    new_version=$((latest_version + 1))
 fi
 
-if [[ "$new_version" != "$latest_version" ]]
-then
-    git tag "$tag_prefix-$new_version-$current_sha"
-    git push --tags > /dev/null
-fi
+git tag "$tag_prefix-$new_version"
+git push --tags > /dev/null
 
 jq -n --arg version "$new_version" '{"version":$version}'
