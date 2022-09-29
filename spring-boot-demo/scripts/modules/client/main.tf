ebeeacf819eaa5edeb3dc3d190921e5c2e7df7b9
Igor Bari <mucsi96@gmail.com>
2022-09-29 21:35:48 +0200

    add required_providers

--- spring-boot-demo/scripts/modules/client/main.tf
+++ spring-boot-demo/scripts/modules/client/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.22.0"
+    }
+  }
+}
+
 module "image_version" {
   source     = "../version"
   tag_prefix = "client-image"
--- spring-boot-demo/scripts/providers.tf
+++ spring-boot-demo/scripts/providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "2.22.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
@@ -15,11 +6,4 @@ provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
-}
-
-provider "docker" {
-  # registry_auth {
-  #   address     = "registry-1.docker.io"
-  #   config_file = pathexpand("~/.docker/config.json")
-  # }
 }
